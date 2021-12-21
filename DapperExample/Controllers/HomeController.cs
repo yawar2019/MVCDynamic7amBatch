9efc68c42a13e2505ce0f89e2a5531d7fff86b95
yawar2019 <yawarali17@gmail.com>
2021-12-21 08:17:50 +0530

    committed

---
+++ DapperExample/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using DapperExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DapperExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            return View(db.getEmployees());
+        }
+    }
+}
---
+++ DapperExample/Models/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+namespace DapperExample.Models
+{
+    public class EmployeeContext
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=AZAM-PC\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=true;");
+        public List<EmployeeModel> getEmployees()
+        {
+            var Employees = con.Query<EmployeeModel>("select EmpId,EmpName,EmpSalary from dbo.employeeDetails").ToList();
+            return Employees;
+        }
+    }
+}
