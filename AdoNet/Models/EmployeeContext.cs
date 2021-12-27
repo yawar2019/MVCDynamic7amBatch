9425285bffebb0f51ba9881c18eaedb8c8fac3bc
yawar2019 <yawarali17@gmail.com>
2021-12-27 08:05:16 +0530

    committed

---
+++ AdoNet/Models/EmployeeContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AdoNet.Models
+{
+    public class EmployeeContext
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlCon"].ToString());
+        public List<EmployeeModel> getEmployeeData()
+        {
+            List<EmployeeModel> listObj = new List<EmployeeModel>();
+            SqlCommand cmd = new SqlCommand("sp_employee", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                EmployeeModel emp = new EmployeeModel();
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+                listObj.Add(emp);
+            }
+
+            return listObj;
+        }
+    }
+}
