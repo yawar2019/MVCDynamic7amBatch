e365107d89508c5a1330e89bcf1d6c6fbb3dd058
yawar2019 <yawarali17@gmail.com>
2022-01-05 08:02:55 +0530

    committed

--- CodeFirstApproach/Controllers/HomeController.cs
+++ CodeFirstApproach/Controllers/HomeController.cs
@@ -15,8 +15,24 @@ namespace CodeFirstApproach.Controllers
         {
             return View(db.EmployeeModels.ToList());
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            db.EmployeeModels.Add(emp);//insert query generated emp values assign
+            //db.departmentModels.Add(dept);//insert query generated emp values assign
+            int i = db.SaveChanges();//similar to execute nonquery
 
-//2
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        
 
     }
 }
